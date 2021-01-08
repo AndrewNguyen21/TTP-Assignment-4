949b7601ecfda08121a938dc371d9e8a27775559
Andrew Nguyen <andrewnguyen2100@gmail.com>
2021-01-08 13:51:36 -0500

    task 1 and task 2 complete

---
+++ script.js
@@ -0,0 +1,15 @@
+//Assignment 4
+
+//q1
+function button1() {
+  let right = document.getElementById("argument");
+  right.innerText = "I'm right!";
+}
+
+function button2() {
+  let right = document.getElementById("argument");
+  right.innerText = "No, I'm right!";
+}
+
+//q2
+//script written in task2.html;
